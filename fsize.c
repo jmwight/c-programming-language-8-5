c7cef84bd8f979ddbdd21beab62c23a0fc483525
John Morgan Wight <john.morgan.wight@proton.me>
2025-05-27 00:47:40 -0400

    Initial commit mostly the same as book
    **UNTESTED**
    
    From the C Programming Language by Dennis Ritchie and Brian Kernigan.
    Some small modifications due to changes from ANSI C (C89) to ISO like
    dirent.h is standard. Also <linux/limits.h> for NAME_MAX and PATH_MAX
    definitions (instead of hard coding them with define statements)

---
+++ fsize.c
@@ -0,0 +1,61 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <fcntl.h> /* Why do I need this? What are AT_* constants in the man page? */
+#include <linux/limits.h> /* where NAME_MAX and PATH_MAX are defined, not from book */
+
+void fsize(char *);
+void dirwalk(char *, void (*)(char *));
+
+int main(int argc, char **argv)
+{
+	if(argc == 1) /* default: current directory */
+		fsize(".");
+	else
+		while(--argc > 0)
+			fsize(*++argv);
+	return 0;
+}
+
+/* fsize: print size of file "name" */
+void fsize(char *name)
+{
+	struct stat stbuf;
+
+	if(stat(name, &stbuf) == -1)
+	{
+		fprintf(stderr, "fsize: can't access %s\n", name);
+		return;
+	}
+	if((stbuf.st_mod & S_IFMT) == S_IFDIR)
+		dirwalk(name, fsize);
+	printf("%8ld %s\n", (long)stbuf.st_size, name);
+}
+
+/* dirwalk: apply fcn to all files in dir */
+void dirwalk(char *dir, void (*fcn)(char *))
+{
+	char name[MAX_PATH];
+	struct dirent *dp;
+	DIR *dfd;
+
+	if((dfd = opendir(dfd)) == NULL)
+	{
+		fprintf(stderr, "dirwalk: can't open %s\n", dir);
+		return;
+	}
+	while((dp = readdir(dfd)) != NULL)
+	{
+		if(strcmp(dp->name, ".") == 0 || strcmp(dp->name, "..") == 0)
+			continue;
+		if(strlen(dir)+strlen(dp->name)+2 > sizeof(name))
+			fprintf(stderr, "dirwalk: name %s/%s too long\n", dir, dp->name);
+		else
+		{
+			sprintf(name, "%s/%s", dir, dp-name);
+			(*fcn)(name);
+		}
+	}
+	closedir(dfd);
+}
